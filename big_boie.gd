e90395e0f681d366fdd2e98774b99003efd2c8b0
Callen Shutts <callenshutts@gmail.com>
2024-06-05 21:50:20 -0400

    added  base function to auto add tiles

--- big_boie.gd
+++ big_boie.gd
@@ -1,1 +1,36 @@
 extends  Node2D
+
+
+# Variables to define grid properties
+var columns := 500
+var rows := 400
+var panel_spacing := 10
+
+# Preload the Panel script
+var PanelScript := preload("res://magic_panel.gd")
+
+func _ready():
+	create_panel_grid()
+
+func create_panel_grid():
+	# Get the size of the Control node
+	var control_size = 100
+	# Calculate the size for each panel
+	var panel_size = Vector2(100,100)
+
+	for row in range(rows):
+		for col in range(columns):
+			var panel: Panel = Panel.new()
+			# Add the panel to the scene tree
+			self.add_child(panel)
+			# Set the size and position of the panel
+			var new_style = StyleBoxFlat.new()
+			new_style.set_bg_color(Color(1, 1, 0, 1))
+			panel.set_size(Vector2(200, 200))
+			panel.set("custom_styles/panel", new_style)
+			panel.set_global_position(Vector2(
+				col * (panel_size.x + panel_spacing),
+				row * (panel_size.y + panel_spacing)
+			))
+			# Attach the Panel script to each panel
+			panel.set_script(PanelScript)
